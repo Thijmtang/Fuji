ba0ab8804ee995475152a61950491e72212e0153
Thijmtang <thijmtang2003@gmail.com>
2021-12-15 17:15:19 +0100

    FInal

--- deleteSong.php
+++ deleteSong.php
@@ -6,11 +6,11 @@ include "header.php";
 $givenSong = $_GET['id'];
 $givenAlbum = $_GET['albumID'];
 
-$songResult = AlbumSongs($conn, $givenAlbum, $givenSong);
+$songResult = AlbumSongs($conn, $givenAlbum, $givenSong, "*");
 $songCount = mysqli_num_rows($songResult);
 
 //only delete if there is a song in that album with same id
-if ($songCount == 1) {
+if ($songCount != 0) {
 
     while ($row = mysqli_fetch_assoc($songResult)) {
         $UpdateStatus = "UPDATE songs SET Active=0 WHERE SONG_ID =$givenSong AND ALBUM =$givenAlbum";
